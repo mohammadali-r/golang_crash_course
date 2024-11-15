815d0c284c382041c639ffdd8c67bd91f2a6b953
MohammadAli <72750128+mohammadali-r@users.noreply.github.com>
2024-11-15 20:45:45 +0330

    Feat/add pointers (#3)
    * feat: add maps directory
    
    * feat: add range directory
    
    * feat: add pointers directory
    
    ---------
    
    Co-authored-by: mohammadalirohanian <rohanian021@gmail.com>

---
+++ pointers/main.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+
+func main(){
+	a := 10
+	b := &a
+	fmt.Println(a, b)
+
+	fmt.Printf("%T\n", b)
+
+	// read value from address
+	println(*b)
+	println(*&a) // actually reference to the a value
+
+	// change value with pointer
+	*b = 20
+	println(a)
+}
