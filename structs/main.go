e06f0d7cd6e887a5ec416e9f08d71e79ef30b30f
MohammadAli <72750128+mohammadali-r@users.noreply.github.com>
2024-11-28 21:26:47 +0330

    feat: add structs directory (#5)
    Co-authored-by: mohammadalirohanian <rohanian021@gmail.com>

---
+++ structs/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type Person struct {
+	// first_name string
+	// last_name  string
+	// gender     string
+	// city       string
+	// age        int
+
+	first_name, last_name, gender, city string
+	age                                 int
+}
+
+// two type method: 1. value receiver (that doesn't chage the values), 2. pointer receiver (that chages the values)
+
+// greet method (value receiver)
+func (p Person) greet() string {
+	return "My name is " + p.first_name + " " + p.last_name + " and I am " + strconv.Itoa(p.age)
+}
+
+// grow_up method (pointer receiver)
+func (p *Person) grow_up() {
+	p.age++
+}
+
+// get_married method (pointer receiver)
+func (p *Person) get_married(spouse_last_name string) {
+	if p.gender == "male" {
+		return
+	} else {
+		p.last_name = spouse_last_name
+	}
+}
+
+func main() {
+	p1 := Person{first_name: "alex", last_name: "williams", gender: "male", city: "hamborg", age: 25}
+	p2 := Person{"sara", "johnson", "female", "new york", 23}
+	fmt.Println(p1)
+	fmt.Println(p2)
+	p1.grow_up()
+	p1.get_married("hugo")
+	fmt.Println(p1.greet())
+	p2.grow_up()
+	p2.get_married("jonas")
+	fmt.Println(p2.greet())
+}
