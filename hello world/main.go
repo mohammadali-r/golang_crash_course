396a0fe971b79d6b5c77bfcac2e0692a6e9d4c6c
mohammadalirohanian <rohanian021@gmail.com>
2024-12-02 20:24:42 +0330

    feat: delete unused files

--- hello world/main.go
+++ hello world/main.go
@@ -1,14 +1,8 @@
 package main
-import "fmt"
-// import "rsc.io/quote/v4"
 
-// func main(){
-//     fmt.Println(quote.Go())
-// }
+import "fmt"
 
 // how to print with fmt package
-func main(){
-    // fmt.Println("Hello world")
-    // fmt.Printf("hello in printf")
-    fmt.Print("hello with print")
+func main() {
+	fmt.Println("hello with print")
 }
