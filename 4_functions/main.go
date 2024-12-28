11fd3812f5dd03742fe9daf6ab70b404b737db01
MohammadAli <72750128+mohammadali-r@users.noreply.github.com>
2024-12-28 22:26:01 +0330

    feat: add variadic functions to 4_functions (#17)
    Co-authored-by: mohammadalirohanian <rohanian021@gmail.com>

--- 4_functions/main.go
+++ 4_functions/main.go
@@ -12,7 +12,22 @@ func get_sum(num1 int, num2 int) int {
 	return num1 + num2
 }
 
+// variadic function
+func all_sum(nums ...int) {
+	fmt.Print(nums, " ")
+	sum := 0
+
+	for _, num := range nums {
+		sum += num
+	}
+	fmt.Println(sum)
+}
+
 func main() {
 	fmt.Println(greeting("MohammadAli"))
 	fmt.Println(get_sum(10, 15))
+	all_sum(1, 3, 5, 3, 2, 8)
+
+	nums := []int{54, 21, 64, 32, 1, 6, 3} // pass a slice to function as argument
+	all_sum(nums...)
 }
