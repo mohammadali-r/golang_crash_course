c5da6fccc05e4e97d1aaa1947d42e707e28290da
mohammadalirohanian <rohanian021@gmail.com>
2025-01-03 22:59:22 +0330

    feat: add recursion to 4_functions

--- 4_functions/main.go
+++ 4_functions/main.go
@@ -23,11 +23,24 @@ func all_sum(nums ...int) {
 	fmt.Println(sum)
 }
 
+
 func main() {
 	fmt.Println(greeting("MohammadAli"))
 	fmt.Println(get_sum(10, 15))
 	all_sum(1, 3, 5, 3, 2, 8)
-
+	
 	nums := []int{54, 21, 64, 32, 1, 6, 3} // pass a slice to function as argument
 	all_sum(nums...)
+
+	// recursion with anonymous function
+	var fact func(n int) int
+	
+	fact = func(n int) int{
+		if n == 0{
+			return 1
+		}
+		return n * fact(n - 1)
+	}
+
+	fmt.Println("Fact 8 is:", fact(8))
 }
